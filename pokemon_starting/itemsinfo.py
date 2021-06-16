ad5c34cd4343222f373e454661a035e500024ffb
Javy47 <javaughnmiller@gmail.com>
2021-06-15 20:53:23 -0400

    updating file path management

--- pokemon_starting/itemsinfo.py
+++ pokemon_starting/itemsinfo.py
@@ -6,10 +6,11 @@ import pathlib
 healing_items = []
 
 #if i run from terminal use ..\healingitems.txt
-f = open("./healingitems.txt", "r")
-for item in  f:
-    name, description, value = item.split(',')
-    healing_items.append({'name': name, 'description': description, 'value': value})
+with open(pathlib.Path.cwd().joinpath('healingitems.txt')) as h_items:
+
+    for item in  h_items:
+        name, description, value = item.split(',')
+        healing_items.append({'name': name, 'description': description, 'value': value})
     
 
 
